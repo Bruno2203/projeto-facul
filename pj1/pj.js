3af38909ef85ff89f6571d046430df4f7a1bb894
Bruno2203 <125492134+Bruno2203@users.noreply.github.com>
2025-10-09 21:31:49 -0300

    Add files via upload

--- pj1/pj.js
+++ pj1/pj.js
@@ -2,24 +2,25 @@ const menos = document.getElementById('menos');
 const mais = document.getElementById('mais');
 const span_numero = document.getElementById('span');
 
-let contador = 0;
+var contador = 1;
 
 span_numero.textContent = contador; 
-
-mais.addEventListener('click', function() {
-    contador++;
-    span_numero.textContent = contador;
-})
-
-if(contador <= 0){
-    menos.disabled = true;    
-}
-
-else if(contador != 0){
-    menos.disabled = false;
-}
 
 menos.addEventListener('click', function() {
     contador--;
     span_numero.textContent = contador;
+    if (contador <= 0) {
+        menos.disabled = true;
+    }
+});
+
+mais.addEventListener('click', function() {
+    contador++;
+    span_numero.textContent = contador;
+    if (contador <= 0) {
+        menos.disabled = false;
+    }
 })
+
+
+
